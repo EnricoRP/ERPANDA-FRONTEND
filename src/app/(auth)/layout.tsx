6617de1fa8c253334e78f78be5a06b0afbea7e34
EnricoRP <enricoriskip@gmail.com>
2025-09-30 17:05:03 +0700

    configurasi tailwind

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,8 +7,7 @@ const layout = async ({ children }: { children: ReactNode }) => {
       <section className="auth-form">
         <div className="auth-box">
           <div className="flex flex-row gap-3">
-            <Image src="/logo-main.svg" alt="logo" width={37} height={37} />
-            <h1 className="text-2xl font-semibold text-white">Erpanda</h1>
+            <Image src="/logo-main.svg" alt="logo" width={150} height={150} />
           </div>
           <div>{children}</div>
         </div>
