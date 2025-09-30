3079618098cbd55691e4acc6fdc188f5c03badbb
EnricoRP <enricoriskip@gmail.com>
2025-09-30 16:20:17 +0700

    feat(constant): menambah file support sebagai tempat konstan variabel

---
+++ src/constants/index.ts
@@ -0,0 +1,37 @@
+export const FIELD_NAMES = {
+  username: "Username",
+  email: "Email",
+  password: "Password",
+};
+
+export const FIELD_TYPES = {
+  username: "text",
+  email: "email",
+  password: "password",
+};
+
+export const sorts = [
+  {
+    value: "oldest",
+    label: "Oldest",
+  },
+  {
+    value: "newest",
+    label: "Newest",
+  },
+];
+
+export const userRoles = [
+  {
+    value: "user",
+    label: "User",
+    bgColor: "bg-[#FDF2FA]",
+    textColor: "text-[#C11574]",
+  },
+  {
+    value: "admin",
+    label: "Admin",
+    bgColor: "bg-[#ECFDF3]",
+    textColor: "text-[#027A48]",
+  },
+];
